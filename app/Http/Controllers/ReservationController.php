641bdbdfd3bd025548e757937d38abd0622abdf3
Darko Vucetic <darko.vucetic7@gmail.com>
2018-12-02 01:51:50 +0100

    pusten mail

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -12,33 +12,7 @@ class ReservationController extends Controller
 {
     public function createReservation(Request $request)
     {
-        function getUserIP()
-        {
-            // Get real visitor IP behind CloudFlare network
-            if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
-                $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-                $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-            }
-            $client  = @$_SERVER['HTTP_CLIENT_IP'];
-            $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
-            $remote  = $_SERVER['REMOTE_ADDR'];
 
-            if(filter_var($client, FILTER_VALIDATE_IP))
-            {
-                $ip = $client;
-            }
-            elseif(filter_var($forward, FILTER_VALIDATE_IP))
-            {
-                $ip = $forward;
-            }
-            else
-            {
-                $ip = $remote;
-            }
-
-            return $ip;
-        }
-        $user_ip = getUserIP();
         // --- Counts numbers of days
 
         /*$pickUpDate = $request->date;
@@ -77,18 +51,18 @@ class ReservationController extends Controller
 
         $secretKey = "6Lc_H3wUAAAAAFrY5a8MEsZ40H2smUDoVIQuRSJ5";
         $secretKeyClient = $_POST['g-recaptcha-response'];
-
-        $respons = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$secretKeyClient."&remoteip=".$user_ip);
+        $remoteip = $_SERVER['REMOTE_ADDR'];
+        $respons = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$secretKeyClient."&remoteip=".$remoteip);
         $respons = json_decode($respons);
-
         if($respons->success && $respons->score > 0.5)
         {
-            var_dump($respons->score);
+            var_dump($respons);
             \Mail::to($user)->send(new reservationEmail($request));
             return "uspjelo";
         }
         else
         {
+            var_dump($respons->score);
             return "fail";
         }
     }
